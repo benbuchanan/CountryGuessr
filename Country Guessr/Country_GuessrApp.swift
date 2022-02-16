6611b746a29195b14fb8f03d0bf3f419bdd1b056
benbuchanan <benbuchanan11@yahoo.com>
2022-02-16 09:10:53 -0500

    Initial Commit

---
+++ Country Guessr/Country_GuessrApp.swift
@@ -0,0 +1,17 @@
+//
+//  Country_GuessrApp.swift
+//  Country Guessr
+//
+//  Created by Ben Buchanan on 2/16/22.
+//
+
+import SwiftUI
+
+@main
+struct Country_GuessrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
